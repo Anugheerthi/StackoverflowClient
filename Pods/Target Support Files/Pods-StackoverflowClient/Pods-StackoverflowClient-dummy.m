eb2ac4f70b673648cef0a9d95901b22766e88295
Anugheerthi <agheerthi94@gmail.com>
2019-11-15 10:58:52 +0530

    Updated project config - iPhone only with portrait mode support. Handled UIScene delegate method available only for iOS 13 version and above device. Integrated 3rd party frameworks using pod dependency - Alamofire.

---
+++ Pods/Target Support Files/Pods-StackoverflowClient/Pods-StackoverflowClient-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_StackoverflowClient : NSObject
+@end
+@implementation PodsDummy_Pods_StackoverflowClient
+@end
---
+++ Pods/Target Support Files/Pods-StackoverflowClientTests/Pods-StackoverflowClientTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_StackoverflowClientTests : NSObject
+@end
+@implementation PodsDummy_Pods_StackoverflowClientTests
+@end
