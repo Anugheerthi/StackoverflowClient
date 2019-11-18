b358051d54e95862b5ce5a91e1c50edf3a89c083
Anugheerthi <agheerthi94@gmail.com>
2019-11-18 18:36:29 +0530

    Added Test cases for QuestionCellViewModel.

--- StackoverflowClientTests/StackoverflowClientTests.swift
+++ StackoverflowClientTests/StackoverflowClientTests.swift
@@ -23,6 +23,18 @@ class StackoverflowClientTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
+    
+    // MARK: SCQuestionCellViewModel Test Case
+    
+    func testQuestionCellViewModel() {
+        let sampleOwner = SCQuestion.SCOwner.init(displayName: "Anug")
+        let sampleQuestion = SCQuestion(tags: ["iOS", "Android"], owner: sampleOwner, score: 10, title: "Test Title")
+        let cellViewModel = SCQuestionCellViewModel(sampleQuestion)
+        XCTAssert(cellViewModel.questionTitle == "Test Title", "Wrong question title text")
+        XCTAssert(cellViewModel.upvoteCount == "10", "Wrong upvote count text")
+        XCTAssert(cellViewModel.tags == ["iOS", "Android"], "Wrong question tags")
+        XCTAssert(cellViewModel.questionTitle == "Test Title", "Wrong question title text")
+    }
 
     func testPerformanceExample() {
         // This is an example of a performance test case.
